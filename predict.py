5840424d1b6e4c263b5bc597cd3783ecd92f3f4f
houriashaibi <62526490+houriashaibi@users.noreply.github.com>
2022-04-23 21:03:39 +0000

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,36 @@
+from keras.models import load_model
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, Conv3D, BatchNormalization, Activation
+from keras import backend as K
+import os
+from PIL import Image
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import pandas as pd
+
+# load model
+model = load_model('brain-tumor-model.h5')
+classes = os.listdir('C:/Users/po/Downloads/Brain-Tumor-Classification/Brain-Tumor-Classification2/Brain-Tumor-Classification-DataSet/Training')
+
+def names(number):
+    if(number == 0):
+        return classes[0]
+    elif(number == 1):
+        return classes[1]
+    elif(number == 2):
+        return classes[2]
+    elif(number == 3):
+        return classes[3]
+
+img = Image.open('C:/Users/po/Downloads/Brain-Tumor-Classification-DataSet/Brain-Tumor-Classification-DataSet/Testing/pituitary_tumor/image(15).jpg')
+x = np.array(img.resize((150,150)))
+x = x.reshape(1,150,150,3)
+answ = model.predict_on_batch(x)
+classification = np.where(answ == np.amax(answ))[1][0]
+imshow(img)
+print(str(answ[0][classification]*100) + '% Confidence This Is ' + names(classification))
